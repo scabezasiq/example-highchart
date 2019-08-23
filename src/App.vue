d22328f3996139f07fe0e40597c7038aa2014b6a
Samuel Cabezas <samuel.cabezas@iqbuild.cl>
2019-08-23 11:21:09 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<template>
+  <div id="app">
+    <div class="logos">
+      <img class="logo-hc" src="./assets/highcharts_logo.png" alt="Highcharts Logo">
+      <img class="logo-vue" src="./assets/vue_logo.png" alt="Vue Logo">
+    </div>
+    <h2>Welcome in Vue Highcharts Wrapper Demo</h2>
+    <hr style="width: 200px; margin: 60px auto;">
+    <h3>Select chart constructor:</h3>
+    <div class="button-grp">
+      <button @click="select('barChart')" :class="{btnActive: selected === 'barChart'}">Bubble Chart</button>
+    </div>
+    <keep-alive>
+      <component :is="currentView"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import BarChart from './components/BarChart'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      selected: 'barChart',
+      currentView: 'barChart'
+    }
+  },
+  components: {
+    barChart: BarChart,
+  },
+  methods: {
+    activate (elem) {
+      this.selected = elem
+    },
+    handler () {
+      var args = arguments
+      for (var arg of args) {
+        if (arg instanceof Function) {
+          arg()
+        }
+      }
+    },
+    select (elem) {
+      this.currentView = elem
+      this.activate(elem)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0 auto;
+  margin-top: 60px;
+  width: 70%;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.row > * {
+  padding: 0 20px;
+}
+.logos {
+  margin: 0 auto;
+  width:50%;
+}
+.logo-hc {
+  width: 70%;
+  height: auto;
+}
+.logo-vue {
+  width: 15%;
+  height: auto;
+}
+.button-grp {
+  margin: 20px 0;
+}
+button {
+  padding: 10px 20px;
+  outline: none;
+  background: #828ea0;
+  color: #fff;
+  font-size: 16px;
+  margin: 5px;
+  border: 0px;
+}
+button:hover {
+  background: #a7aeb8;
+  transition: background 0.2s;
+}
+button:active {
+  color: rgb(117, 117, 117);
+}
+.btnActive {
+  color: #6fcd98
+}
+</style>
---
+++ src/components/BarChart.vue
@@ -0,0 +1,93 @@
+<template>
+  <highcharts :options="chartOptions" class="map"></highcharts>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      chartOptions: {
+        chart: {
+          type: 'column',
+          inverted: true,
+          padding: 0,
+          backgroundColor: 'rgba(255, 255, 255, 0.0)',
+          alignTicks: false,
+        },
+        title: {
+          text: null,
+        },
+        subtitle: {
+          text: null,
+        },
+        xAxis: {
+          categories: ['Factor de Avance'],
+          lineWidth: 0,
+          tickWidth: 0,
+          tickInterval: 1,
+          labels: {
+            enabled: false,
+          },
+        },
+        yAxis: {
+          title: false,
+          lineWidth: 0,
+          tickWidth: 0,
+          minorTickInterval: 0.1,
+          gridLineWidth: 0,
+          minorGridLineWidth: 0,
+          labels: {
+            enabled: false,
+          },
+          min: 0,
+          max: 100,
+          isDirty: true,
+        },
+        plotOptions: {
+          series: {
+            marker: {
+              enabled: false,
+            },
+          },
+          column: {
+            grouping: false,
+            shadow: false,
+            borderWidth: 0,
+            borderRadius: 2,
+          },
+        },
+        series: [
+          {
+            name: '1',
+            color: 'rgba(255, 255, 255, 0.0)',
+            borderColor: 'blue',
+            borderWidth: 1,
+            data: [100],
+            pointPadding: 0.2,
+          },
+          {
+            name: '2',
+            color: 'red',
+            data: [50],
+            pointPadding: 0.415,
+            pointPlacement: -0.08,
+          },
+          {
+            name: '3',
+            color: 'black',
+            data: [70],
+            pointPadding: 0.415,
+            pointPlacement: 0.065,
+          },
+        ],
+      }
+    }
+  }
+}
+</script>
+ <style scoped>
+.map {
+  min-height: 500px;
+}
+ </style>
